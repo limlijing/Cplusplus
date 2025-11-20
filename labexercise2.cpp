f1f1c6d1f19691d0c8fc4e49ab5aafd66e743cb5
LIM LI JING <limlijing@graduate.utm.my>
2025-11-20 22:28:35 +0800

    Add files via upload

---
+++ labexercise2.cpp
@@ -0,0 +1,22 @@
+//LIM LI JING A25CS0248//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int N,i=1;
+    cout << "Enter the number of lines: ";
+    cin>>N;
+    while(i<=N)
+    {
+        int j=1;
+        while(j<=1)
+        {
+            cout<<j<<" ";
+            j++;
+        }
+           cout<<"\n";
+           i=i+1;
+    }
+    return 0;
+}
